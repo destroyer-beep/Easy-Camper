bcde70d30abc9dbb4585c64d5c8dbca577a28a62
Vladimir <destroyer.hramov@gmail.com>
2021-11-23 02:38:16 +0300

    js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const dialogButton = document.querySelector('.header__profile_button');
 const dialogWindow = document.querySelector('.header__window');
+const titleButton = document.querySelector('.title__button_text');
 
 dialogButton.addEventListener('click', () => {
   dialogWindow.classList.toggle('header__window_active');
   dialogButton.classList.toggle('header__profile_active');
 })
+
+titleButton.addEventListener('click', () => {
+  document.location.href = "http://www.yandex.ru";
+})
+
+
